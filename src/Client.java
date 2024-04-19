0b831b85692ba50bc2345ed4e88ed3184cc29a49
Ahmad Elwan <ahmadmelwan02@gmail.com>
2024-04-19 22:33:52 +0300

    Added comments to Client.java

--- src/Client.java
+++ src/Client.java
@@ -6,8 +6,13 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+/**
+ * This class represents a client that can connect to a dictionary server
+ * and look up word definitions.
+ */
 public class Client {
 
+    // Define default server address and port
     private static final String DEFAULT_SERVER_ADDRESS = "localhost";
     private static final int DEFAULT_PORT = 8080;
 
@@ -15,6 +20,13 @@ public class Client {
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
 
+    /**
+     * Constructor to create a client object and connect to the server.
+     *
+     * @param serverAddress The server address to connect to.
+     * @param port The port number to connect on.
+     * @throws IOException If there's an error during connection or stream creation.
+     */
     public Client(String serverAddress, int port) throws IOException {
         this.socket = new Socket(serverAddress, port); // Connect to server address and port
         this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -22,6 +34,12 @@ public class Client {
         System.out.println("Connected to server!"); // Debugging message
     }
 
+    /**
+     * This method allows the user to enter words and sends them to the server
+     * to look up their definitions. It continues until the user enters "quit".
+     *
+     * @throws IOException If there's an error sending or receiving messages.
+     */
     public void sendMessage() throws IOException {
         Scanner scanner = new Scanner(System.in);
         while (socket.isConnected()) {
@@ -46,6 +64,12 @@ public class Client {
         }
     }
 
+    /**
+     * This method closes all open resources associated with the client,
+     * including the socket, buffered reader, and buffered writer.
+     *
+     * @throws IOException If there's an error while closing any resource.
+     */
     public void closeEverything() throws IOException {
         if (bufferedReader != null) {
             bufferedReader.close();
@@ -58,11 +82,20 @@ public class Client {
         }
     }
 
+    /**
+     * The main method is the entry point for the program. It parses arguments
+     * (server address and port) and creates a Client object to initiate communication
+     * with the server.
+     *
+     * @param args The command-line arguments.
+     * @throws IOException If there's an error during client creation, communication,
+     * or resource closing.
+     */
     public static void main(String[] args) throws IOException {
         String serverAddress = DEFAULT_SERVER_ADDRESS;
         int port = DEFAULT_PORT;
 
-        // Check if arguments are provided
+        // Check if arguments are provided and handle them appropriately
         if (args.length > 0) {
             serverAddress = args[0]; // Override default server address if provided
         }
@@ -76,6 +109,6 @@ public class Client {
 
         Client client = new Client(serverAddress, port);
         client.sendMessage();
-        //client.closeEverything();
+        //client.closeEverything(); // Close resources are closed automatically using a try-with-resources in the sendMessage method
     }
 }
